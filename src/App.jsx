40bf06eb08a1a5d58f1515a2b30007ed34cef2eb
Fatima Rahimi <rahimifatima432@gmail.com>
2025-09-15 10:54:00 +0430

    create first heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App(){
   return(
-    <h1 className="bg-red-500 text-center font-bold">Hellow world</h1>
+    <h1 className="bg-red-500 text-center font-bold p-3 rounded-lg text-white">Hellow world</h1>
   )
 }
 
